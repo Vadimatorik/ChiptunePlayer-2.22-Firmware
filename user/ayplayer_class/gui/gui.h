8e6776cc70cb426ec2536075ba79cbdffda7c816
Vadimatorik <Vadimatorikda@gmail.com>
2018-09-27 21:20:54 +0700

    Убрал дублирование.

--- user/ayplayer_class/gui/gui.h
+++ user/ayplayer_class/gui/gui.h
@@ -13,32 +13,6 @@
 
 namespace AyPlayer {
 
-
-class WindowIndexingSupportedFiles {
-public:
-	WindowIndexingSupportedFiles	(	const MakiseStyle_SList*		style,
-										const MakiseStyle_SListItem*	itemsStyle,
-										MContainer*						c,
-										int32_t							x,
-										int32_t							y,
-										uint32_t						w,
-										uint32_t						h				);
-
-	void setActualState ( const char* const stateMessage );
-	void addItem ( const char* const itemName );
-	~WindowIndexingSupportedFiles();
-
-private:
-	const uint32_t cout	=	4;
-
-private:
-	MSList					sl;
-	MSList_Item				slItem[ 4 ];
-	char*					str;
-
-};
-
-
 class Gui {
 public:
 	Gui	(	const AyPlayerPcbStrcut*				const pcbObj,
