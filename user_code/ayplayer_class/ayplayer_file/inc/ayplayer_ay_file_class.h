506c683df0a7f63d46328a3ac8758edb002d7d1c
Vadim Deryabkin <Vadimatorikda@gmail.com>
2019-06-27 16:50:56 +0700

    Начал переходить на сторонние контейнеры форматов.

--- user_code/ayplayer_class/ayplayer_file/inc/ayplayer_ay_file_class.h
+++ user_code/ayplayer_class/ayplayer_file/inc/ayplayer_ay_file_class.h
@@ -7,62 +7,60 @@
 #include <memory>
 #include "user_os.h"
 
-#define AY_YM_FILE_PLAY_FLASH_BUFFER_SIZE_BYTE				1024 * 10
+#define AY_YM_FILE_PLAY_FLASH_BUFFER_SIZE_BYTE                1024 * 10
 
 struct AyYmFilePlayCfg {
-	AyYmLowLavel*							ayLow;
-	void	( *setPwr )						(	uint32_t			chipNumber,
-												bool				state	);
+    AyYmLowLavel *ayLow;
+    void ( *setPwr ) (uint32_t chipNumber,
+                      bool state);
 };
 
-class AyYmFilePlay : public AyYmFilePlayBase {
+class AyYmFilePlay : public aym_base_parse {
 public:
-	AyYmFilePlay( const AyYmFilePlayCfg* const cfg );
+    AyYmFilePlay (const AyYmFilePlayCfg *const cfg);
 
 
 
-	///void	setUsingChip				(	uint32_t	chipNumber	);
+    ///void	setUsingChip				(	uint32_t	chipNumber	);
 
 
-	void	setPause					(	bool	state	);
-	void	stop						(	void	);
-
-private:
-	int	getFileLen						(	uint32_t&		returnFileLenByte	);
-	void	resetFlags					(	void	);
-	int	openFile						(	std::shared_ptr< char >		fullFilePath	);
-	int	closeFile						(	void	);
-
-	int	setOffsetByteInFile				(	const uint32_t	offsetByte	);
-	int	readInArray						(	uint8_t*		returnDataBuffer,
-											const uint32_t	countByteRead	);
-	int	setPwrChip						(	bool			state	);
-	int	initChip						(	void	);
-	int	sleepChip						(	const uint32_t	countTick	);
-	int	writePacket						(	const uint8_t	reg,
-											const uint8_t	data	);
-
-	void	abort						(	void	);
+    void setPause (bool state);
+    void stop (void);
 
 private:
-	const AyYmFilePlayCfg* const cfg;
-	std::shared_ptr< FIL >				file;
-	AyPlayer::Fat						fat;
+    int get_len_file (uint32_t &len);
+    int open_file (std::shared_ptr<char> f);
+    int close_file (void);
+    int set_offset (const uint32_t offset);
+    int read (uint8_t *buf, const uint32_t num);
+    int set_pwr_chip (const bool state);
+    int init_chip (void);
+    int sleep (const uint32_t num);
+    int set_reg (const uint8_t reg, const uint8_t data);
 
-	uint32_t				usingChip		=	0;
-	bool					flagStop;
+private:
+    void resetFlags (void);
+    void abort (void);
 
-	/// Т.к. методы зачастую читают по 1 байту, то чтобы ускорить этот процесс
-	/// сразу копируется значительный кусок трека.
-	__attribute__((__aligned__(4)))
-	uint8_t					flashBuffer[ AY_YM_FILE_PLAY_FLASH_BUFFER_SIZE_BYTE ];
+private:
+    const AyYmFilePlayCfg *const cfg;
+    std::shared_ptr<FIL> file;
+    AyPlayer::Fat fat;
 
-	/// Смещение, с которого было скопирован кусок.
-	uint32_t				pointStartSeekBuffer;
+    uint32_t usingChip = 0;
+    bool flagStop;
 
-	/// Смещение, с которого будет считан следующий байт/последовательность
-	/// байт (относительно буффера).
-	uint32_t				pointInBuffer;
+    /// Т.к. методы зачастую читают по 1 байту, то чтобы ускорить этот процесс
+    /// сразу копируется значительный кусок трека.
+    __attribute__((__aligned__(4)))
+    uint8_t flashBuffer[AY_YM_FILE_PLAY_FLASH_BUFFER_SIZE_BYTE];
+
+    /// Смещение, с которого было скопирован кусок.
+    uint32_t pointStartSeekBuffer;
+
+    /// Смещение, с которого будет считан следующий байт/последовательность
+    /// байт (относительно буффера).
+    uint32_t pointInBuffer;
 };
 
 
