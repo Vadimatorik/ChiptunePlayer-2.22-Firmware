cf6a8545ffe80009c32b997ce9048b8aef7261fe
Vadimatorik <Deryabkinda@mail.ru>
2020-01-14 00:11:28 +0700

    changelog: в lua криптах 1. Добавлены методы для чтения строки и int из файла. 2. Теперь возвращаемое значение ошибки из библиотеки fat - отрицательное число.

--- lua_scripts/file/base_func.lua
+++ lua_scripts/file/base_func.lua
@@ -1,9 +1,9 @@
 function open_file (path_to_dir, file_name, fat_fs_file_obj, open_flags)
     local path = path_to_dir .. file_name
-    log("Start open file. Path: " .. path)
+    log("Start open file. Path file: " .. path)
     local rv = fat_fs_file_obj:open(path, open_flags)
     if rv ~= 0 then
-        log_err("Fail open file. Err: " .. get_fat_fs_err_str(rv) .. ". Path: " .. path)
+        log_err("Fail open file. Err: " .. get_fat_fs_err_str(rv) .. ". Path file: " .. path)
     end
 
     return rv
@@ -11,10 +11,10 @@ end
 
 function close_file (path_to_dir, file_name, fat_fs_file_obj)
     local path = path_to_dir .. file_name
-    log("Start close file. Path: " .. path)
+    log("Start close file. Path file: " .. path)
     local rv = fat_fs_file_obj:close()
     if rv ~= 0 then
-        log_err("Fail close file. Err: " .. get_fat_fs_err_str(rv) .. ". Path: " .. path)
+        log_err("Fail close file. Err: " .. get_fat_fs_err_str(rv) .. ". Path file: " .. path)
     end
 
     return rv
@@ -22,11 +22,11 @@ end
 
 function lseek_file (path_to_dir, file_name, fat_fs_file_obj, lseek_byte)
     local path = path_to_dir .. file_name
-    log("Start lseek file. Seek: " .. tostring(lseek_byte) .. " byte. Path: " .. path)
+    log("Start lseek file. Seek: " .. tostring(lseek_byte) .. " byte. Path file: " .. path)
     local rv = fat_fs_file_obj:lseek(lseek_byte)
     if rv ~= 0 then
         local err_str = "Fail lseek file. Err: " .. get_fat_fs_err_str(rv) .. ". "
-        err_str = err_str .. "Seek: " .. tostring(lseek_byte) .. " byte. Path: " .. path
+        err_str = err_str .. "Seek: " .. tostring(lseek_byte) .. " byte. Path file: " .. path
         log_err(err_str)
     end
 
@@ -35,11 +35,11 @@ end
 
 function write_file_string (path_to_dir, file_name, fat_fs_file_obj, str)
     local path = path_to_dir .. file_name
-    log("Start write string to file. Str: " .. str .. ". Path: " .. path)
+    log("Start write string to file. Str: " .. str .. ". Path file: " .. path)
     local rv = fat_fs_file_obj:write_string(str)
 	if rv ~= 0 then
 		local err_str = "Fail write string to file. Str: " .. str .. ". Err: " .. get_fat_fs_err_str(rv) .. ". "
-		err_str = err_str .. "Path: " .. path
+		err_str = err_str .. "Path file: " .. path
 		log_err(err_str)
     end
 
@@ -48,14 +48,43 @@ end
 
 function write_file_int (path_to_dir, file_name, fat_fs_file_obj, var)
 	local path = path_to_dir .. file_name
-	log("Start write int to file. Path: " .. path)
+	log("Start write int to file. Path file: " .. path)
 	local rv = fat_fs_file_obj:write_int(var)
 	if rv ~= 0 then
 		local err_str = "Fail write int to file. Var: " .. tostring(var) .. ". Err: " .. get_fat_fs_err_str(rv) .. ". "
-		err_str = err_str .. "Path: " .. path
+		err_str = err_str .. "Path file: " .. path
 		log_err(err_str)
 	end
 
 	return rv
 end
 
+function read_file_string (path_to_dir, file_name, fat_fs_file_obj, ret_str)
+    local path = path_to_dir .. file_name
+    log("Start read string from file. Path file: " .. path)
+    local rv = fat_fs_file_obj:read_string()
+    if type(rv) == "number" then
+        local err_str = "Fail read string from file. Err: " .. get_fat_fs_err_str(rv) .. ". "
+        err_str = err_str .. "Path file: " .. path
+        log_err(err_str)
+        return rv
+    else
+        ret_str = rv
+        return 0
+    end
+end
+
+function read_file_int (path_to_dir, file_name, fat_fs_file_obj, ret_var)
+    local path = path_to_dir .. file_name
+    log("Start read int from file. Path file: " .. path)
+    local rv = fat_fs_file_obj:read_int()
+    if rv < 0 then
+        local err_str = "Fail read int from file. Var: " .. tostring(var) .. ". Err: " .. get_fat_fs_err_str(rv) .. ". "
+        err_str = err_str .. "Path file: " .. path
+        log_err(err_str)
+        return rv
+    else
+        ret_var = rv
+        return 0
+    end
+end
--- lua_scripts/file/list/base_func.lua
+++ lua_scripts/file/list/base_func.lua
@@ -37,9 +37,11 @@ function write_fil_list_item (path_to_dir, file_name, fat_fs_file_obj, fil_item_
     end
 
     rv = write_file_string(path_to_dir, file_name, fat_fs_file_obj, file_data.name)
+    if rv ~= 0 then
+        return rv
+    end
 
     -- Len
-	lseek_byte = fil_item_number * list_fil_item_len.sum_len
 	lseek_byte = lseek_byte + list_fil_item_len.name
     rv = lseek_file(path_to_dir, file_name, fat_fs_file_obj, lseek_byte)
     if rv ~= 0 then
@@ -47,6 +49,40 @@ function write_fil_list_item (path_to_dir, file_name, fat_fs_file_obj, fil_item_
     end
 
     rv = write_file_int(path_to_dir, file_name, fat_fs_file_obj, file_data.len)
+    return rv
+end
+
+function read_dir_list_item (path_to_dir, file_name, fat_fs_file_obj, dir_item_number, ret_dir_name)
+    local lseek_byte = dir_item_number * list_dir_item_len.sum_len
+    local rv = lseek_file(path_to_dir, file_name, fat_fs_file_obj, lseek_byte)
+    if rv ~= 0 then
+        return rv
+    end
+
+    return read_file_string(path_to_dir, file_name, fat_fs_file_obj, ret_dir_name)
+end
+
+function read_fil_list_item (path_to_dir, file_name, fat_fs_file_obj, fil_item_number, ret_file_data)
+    -- Name
+    local lseek_byte = fil_item_number * list_fil_item_len.sum_len
+    local rv = lseek_file(path_to_dir, file_name, fat_fs_file_obj, lseek_byte)
+    if rv ~= 0 then
+        return rv
+    end
+
+    rv = read_file_string(path_to_dir, file_name, fat_fs_file_obj, ret_file_data.name)
+    if rv ~= 0 then
+        return rv
+    end
+
+    -- Len
+	lseek_byte = lseek_byte + list_fil_item_len.name
+    rv = lseek_file(path_to_dir, file_name, fat_fs_file_obj, lseek_byte)
+    if rv ~= 0 then
+        return rv
+    end
+
+    rv = read_file_int(path_to_dir, file_name, fat_fs_file_obj, ret_file_data.len)
     return rv
 end
 
