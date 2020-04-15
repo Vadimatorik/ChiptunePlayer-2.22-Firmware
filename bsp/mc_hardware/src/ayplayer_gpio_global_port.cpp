956e2fb9b59dbcd0125be9edb5833d508cb3dfc2
Vadimatorik <Vadimatorikda@gmail.com>
2020-04-15 17:03:25 +0700

    Размещено предупреждение, что прошлая версия платы была неудачной.

--- bsp/mc_hardware/src/ayplayer_gpio_global_port.cpp
+++
@@ -1,5 +0,0 @@
-#include "ayplayer_mc_hardware.h"
-
-extern const McHardwareInterfacesImplementation::PinCfg gpCfg[ 35 ];
-
-McHardwareInterfacesImplementation::PortGlobal gp( gpCfg, M_SIZE_ARRAY( gpCfg ) );
--- cmake_functions.cmake
+++
@@ -1,7 +0,0 @@
-function(write_sector SECTOR ADDR_BASE ADDR_START ADDR_END)
-    add_custom_command(TARGET ${PROJECT_NAME}.elf POST_BUILD
-            COMMAND ${ARM_OBJCOPY} --output-target=binary --gap-fill=0xff ${PROJECT_BINARY_DIR}/${PROJECT_NAME}.elf ${PROJECT_BINARY_DIR}/${PROJECT_NAME}_all.bin
-            COMMENT "Creating a binary file of the <<${SECTOR}>> sector"
-            COMMAND ${BIN_EXTRACTOR} -p ${PROJECT_BINARY_DIR}/${PROJECT_NAME}_all.bin -o ${PROJECT_BINARY_DIR}/${PROJECT_NAME}_section_${SECTOR}_new.bin -b ${ADDR_BASE} -s ${ADDR_START} -e ${ADDR_END}
-            COMMAND cd ${CMAKE_SOURCE_DIR} && ./cmp.sh ${PROJECT_BINARY_DIR}/${PROJECT_NAME}_section_${SECTOR}.bin ${PROJECT_BINARY_DIR}/${PROJECT_NAME}_section_${SECTOR}_new.bin "${STM32PROG} -c port=${STM32PROG_PORT} freq=${STM32PROG_FREQ} -w ${PROJECT_BINARY_DIR}/${PROJECT_NAME}_section_${SECTOR}.bin ${ADDR_START}")
-endfunction(write_sector)
